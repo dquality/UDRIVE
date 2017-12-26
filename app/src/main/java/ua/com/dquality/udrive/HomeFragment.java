39f48c7493578a85489d984925973caec488967d
dquality <pasha.rudenok@gmail.com>
2017-12-26 02:04:01 +0200

    Start working on Home page layout

--- app/src/main/java/ua/com/dquality/udrive/HomeFragment.java
+++ app/src/main/java/ua/com/dquality/udrive/HomeFragment.java
@@ -1,7 +1,10 @@
 package ua.com.dquality.udrive;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,11 +14,24 @@ public class HomeFragment extends Fragment {
     public HomeFragment() {
     }
 
+    private ViewPager mPager;
+    private PagerAdapter mPagerAdapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        mPager = (ViewPager)  (getView().findViewById(R.id.homeViewPager));
+        mPagerAdapter = new HomeSwipeFragmentPageAdapter(getChildFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+        mPager.setPageTransformer(true, new ZoomOutPageTransformer());
     }
 }
 
