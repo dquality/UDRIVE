c43ffc57556a2b5e00749284ed9e64c76bcd9b4a
Pavlo Rudenok <Pavlo.Rudenok@infopulse.com>
2018-03-23 01:22:49 +0200

    Add calendar date range

---
+++ app/src/main/java/ua/com/dquality/udrive/fragments/DateRangePickerFragment.java
@@ -0,0 +1,104 @@
+package ua.com.dquality.udrive.fragments;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.DatePicker;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
+import java.util.Calendar;
+
+import ua.com.dquality.udrive.R;
+
+import static com.prolificinteractive.materialcalendarview.MaterialCalendarView.SELECTION_MODE_RANGE;
+
+public class DateRangePickerFragment extends DialogFragment {
+
+    private CalendarDay mStartDate;
+    private CalendarDay mEndDate;
+    private Calendar mCalendar;
+    private com.prolificinteractive.materialcalendarview.MaterialCalendarView mCalendarView;
+
+    private DateRangePickerFragment.OnDateRangeSetListener mDateRangeSetListener;
+
+    public void initDateRangePickerFragment(DateRangePickerFragment.OnDateRangeSetListener listener, CalendarDay singleWeekDay){
+        mDateRangeSetListener = listener;
+        mCalendar = Calendar.getInstance();
+        if(singleWeekDay == null)
+        {
+            singleWeekDay = CalendarDay.from(mCalendar);
+        }
+        calculateStartEndWeekDate(singleWeekDay);
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        Activity activity = getActivity();
+
+        LayoutInflater layoutInflater = activity.getLayoutInflater();
+        View viewContent = layoutInflater.inflate(R.layout.statement_week_veiw, null);
+
+        AlertDialog dialog = new AlertDialog.Builder(activity)
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                if(mDateRangeSetListener !=null)
+                                    mDateRangeSetListener.onDateRangeSet(mCalendarView, mStartDate, mEndDate);
+                            }
+                        }
+                )
+                .setNegativeButton("Cancel", null)
+                .setView(viewContent)
+                .create();
+
+        mCalendarView = viewContent.findViewById(R.id.statement_week_view);
+        mCalendar.setFirstDayOfWeek(mCalendarView.getFirstDayOfWeek());
+        mCalendarView.setSelectionMode(SELECTION_MODE_RANGE);
+        mCalendarView.selectRange(mStartDate, mEndDate);
+        mCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+
+                calculateStartEndWeekDate(date);
+                widget.clearSelection();
+                if(selected){
+                    widget.selectRange(mStartDate, mEndDate);
+                }
+            }
+        });
+
+        return dialog;
+    }
+
+    private void calculateStartEndWeekDate(CalendarDay date){
+        mCalendar.set(date.getYear(), date.getMonth(), date.getDay());
+
+        mCalendar.get(Calendar.DAY_OF_WEEK);
+
+        int firstDayOfWeek = mCalendar.getFirstDayOfWeek();
+        mCalendar.set(Calendar.DAY_OF_WEEK, firstDayOfWeek);
+        mStartDate = CalendarDay.from(mCalendar.get(Calendar.YEAR), mCalendar.get(Calendar.MONTH), mCalendar.get(Calendar.DATE));
+        mCalendar.set(Calendar.DAY_OF_WEEK, firstDayOfWeek + 6);
+        mEndDate = CalendarDay.from(mCalendar.get(Calendar.YEAR), mCalendar.get(Calendar.MONTH), mCalendar.get(Calendar.DATE));
+    }
+
+    public interface OnDateRangeSetListener {
+        /**
+         * @param widget the picker associated with the dialog
+         * @param startDate date from
+         * @param endDate date to
+         */
+        void onDateRangeSet(MaterialCalendarView widget, CalendarDay startDate, CalendarDay endDate);
+    }
+}
--- app/src/main/java/ua/com/dquality/udrive/fragments/ProfitStatementFragment.java
+++ app/src/main/java/ua/com/dquality/udrive/fragments/ProfitStatementFragment.java
@@ -1,35 +1,37 @@
 package ua.com.dquality.udrive.fragments;
 
 
+import android.app.DatePickerDialog;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.widget.ExpandableListView;
 
-import java.util.List;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
 import ua.com.dquality.udrive.R;
-import ua.com.dquality.udrive.helpers.ExpandableProfitHistoryAdapter;
 import ua.com.dquality.udrive.helpers.ExpandableProfitStatementAdapter;
-import ua.com.dquality.udrive.viewmodels.ProfitHistoryGroupModel;
-import ua.com.dquality.udrive.viewmodels.ProfitHistoryViewModel;
 import ua.com.dquality.udrive.viewmodels.ProfitStatementModel;
 import ua.com.dquality.udrive.viewmodels.ProfitStatementViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ProfitStatementFragment extends Fragment {
+public class ProfitStatementFragment extends Fragment implements DateRangePickerFragment.OnDateRangeSetListener, View.OnClickListener {
 
 
     private ProfitStatementViewModel mViewModelData;
     private ExpandableProfitStatementAdapter mExpandableListAdapter;
     private ExpandableListView mExpandableListView;
+    private AppCompatButton mPickDateButton;
 
     public ProfitStatementFragment() {
         // Required empty public constructor
@@ -55,8 +57,22 @@ public class ProfitStatementFragment extends Fragment {
             }
         });
 
+        mPickDateButton = ret.findViewById(R.id.profit_statement_pick_date_button);
+        mPickDateButton.setOnClickListener(this);
+
         return ret;
     }
 
+    @Override
+    public void onDateRangeSet(MaterialCalendarView view, CalendarDay startDate, CalendarDay endDate) {
+        //mPickDateButton.setText(String.format("Year: %d  Month: %d Day: %d",  year, month, dayOfMonth));
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        DateRangePickerFragment newFragment = new DateRangePickerFragment();
+        newFragment.initDateRangePickerFragment(this, null);
+        newFragment.show(getActivity().getSupportFragmentManager(), "statementPeriodDatePicker");
+    }
 }
